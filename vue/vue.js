33b2635741cd1c9cd1cf7b809e09f663fa872a02
Joel Silva <joelscatarino@outlook.com>
2021-08-21 18:47:45 -0300

    combat_1

--- vue/vue.js
+++ vue/vue.js
@@ -11,9 +11,28 @@ new Vue({
         }
     },
     methods: {
-
+        startGame() {
+            this.running = true
+            this.ken = 100
+            this.ryu = 100
+        },
+        attack(especial) {
+            this.hurt('ken', 5, 10, especial)
+            this.hurt('ryu', 7, 12, false)
+        },
+        hurt(prop, min, max, especial) {
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        }
     },
     watch: {
-
+        hasResult(value) {
+            if (value) this.running = false
+        }
     }
 })
