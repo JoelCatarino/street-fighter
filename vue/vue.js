744a7604058e584f67c3aa0175f095068c5e34c2
Joel Silva <joelscatarino@outlook.com>
2021-08-21 15:56:48 -0300

    commit

--- vue/vue.js
+++ vue/vue.js
@@ -1,10 +1,14 @@
-new Vue ({
+new Vue({
     el: '#app',
     data: {
-
+        running: false,
+        ryu: 100,
+        ken: 100
     },
     computed: {
-
+        hasResult() {
+            return this.ryu == 0 || this.ken == 0
+        }
     },
     methods: {
 
