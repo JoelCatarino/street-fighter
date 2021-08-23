634be9d4b23a30ef77dd8f6544fbb1a576ae4860
Joel Silva <joelscatarino@outlook.com>
2021-08-23 15:25:34 -0300

    Final_commit

--- vue/vue.js
+++ vue/vue.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         running: false,
         ryu: 100,
-        ken: 100
+        ken: 100,
+        logs: []
     },
     computed: {
         hasResult() {
@@ -15,19 +16,35 @@ new Vue({
             this.running = true
             this.ken = 100
             this.ryu = 100
+            this.logs = []
         },
         attack(especial) {
-            this.hurt('ken', 5, 10, especial)
-            this.hurt('ryu', 7, 12, false)
+            this.hurt('ken', 5, 10, especial, 'ryu', 'ken', 'player')
+            if (this.ken > 0) {
+                this.hurt('ryu', 7, 12, false, 'ken', 'ryu', 'masters')
+            }
         },
-        hurt(prop, min, max, especial) {
+        hurt(prop, min, max, especial, source, target, cls) {
             const plus = especial ? 5 : 0
             const hurt = this.getRandom(min + plus, max + plus)
             this[prop] = Math.max(this[prop] - hurt, 0)
+            this.registerLog(`${source} reached ${target} with ${hurt}.`, cls)
+        },
+        heal(min, max) {
+            const heal = this.getRandom(min, max)
+            this.ryu = Math.min(this.ryu + heal, 100)
+            this.registerLog(`Player gained strength from ${heal}.`, 'player')
+        },
+        healAndHurt() {
+            this.heal(10, 15)
+            this.hurt('ryu', 7, 12, false, 'ken', 'ryu', 'ken')
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
+        },
+        registerLog(text, cls) {
+            this.logs.unshift({ text, cls })
         }
     },
     watch: {
